55470a9c6aa28ecca42923a504f5bd34b994b0e9
Isaac Ajayi <ajayioisaac@gmail.com>
2023-04-24 17:44:40 +0100

    now

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
-"""Returns information about his/her TODO list progress."""
+"""Returns to-do list information for a given employee ID."""
 import requests
-from sys import argv
+import sys
 
+if len(sys.argv) != 2:
+    print(f"Usage: python3 {sys.argv[0]} EMPLOYEE_ID")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+employee_id = sys.argv[1]
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-          len(completed), len(todos)))
-    [print("\t {}".format(com)) for com in completed]
+url_base = "https://jsonplaceholder.typicode.com"
+url_user = f"{url_base}/users/{employee_id}"
+url_todos = f"{url_base}/todos?userId={employee_id}"
+
+response_user = requests.get(url_user)
+response_todos = requests.get(url_todos)
+
+if response_user.status_code != 200 or response_todos.status_code != 200:
+    print("Error: Failed to retrieve data from the API")
+    sys.exit(1)
+
+user_data = response_user.json()
+todos_data = response_todos.json()
+
+employee_name = user_data["name"]
+total_tasks = len(todos_data)
+completed_tasks = len([todo for todo in todos_data if todo["completed"]])
+completed_tasks_titles = [todo["title"] for todo in todos_data if todo["completed"]]
+
+print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+for title in completed_tasks_titles:
+    print(f"\t {title}")
