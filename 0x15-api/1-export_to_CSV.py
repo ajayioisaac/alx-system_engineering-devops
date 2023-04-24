2c041ff9d90d73462fea8027e82a1a0413b3dbf7
Isaac Ajayi <ajayioisaac@gmail.com>
2023-04-24 17:51:31 +0100

    add

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,17 +2,18 @@
 """Python script to export data in the CSV format."""
 import csv
 import requests
-from sys import argv
+from sys
 
 
 if __name__ == "__main__":
+    u_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(argv[1])).json()
+    user = requests.get(url + "users/{}".format(u_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": u_id}).json()
 
-    with open("{}.csv".format(argv[1]), "w") as csvfile:
+    with open("{}.csv".format(u_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [argv[1], username, to.get("completed"), to.get("title")])
-            for to in todos]
+            [u_id, username, t.get("completed"), t.get("title")])
+            for t in todos]
